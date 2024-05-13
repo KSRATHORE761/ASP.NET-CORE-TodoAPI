3e75a09d2c0c9f5de90f6b8cc54521e87a9048ec
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2024-05-13 16:29:58 +0530

    TodoAPI

---
+++ DAL/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Microsoft.Extensions.Options;
+
+namespace DAL
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base (options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public string? Secret { get; set; }
+
+    }
+}
